3262f71da8e308fd632bb12234968cf544e8162b
mikkusu <yuzuponikemi@gmail.com>
2021-11-03 11:45:35 +0900

    predictOneVsAll check

--- machine-learning-ex3/predictOneVsAll.m
+++ machine-learning-ex3/predictOneVsAll.m
@@ -13,6 +13,7 @@ num_labels = size(all_theta, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
+maximum = zeros(size(X, 1), 1);
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
@@ -32,7 +33,7 @@ X = [ones(m, 1) X];
 
 
 
-
+[maximum, p] = max(X*all_theta', [], 2);
 
 
 
